196972e06d9f5e6a324d60d4ca4bab81c8d17a25
tadeo <tadeo.mej@gmail.com>
2022-06-20 16:47:47 -0500

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product.component';
@@ -33,4 +32,5 @@ import { DemoComponent } from './demo/demo.component';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
