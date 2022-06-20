fea614526579a42d7077014110c2d583b8fbd20d
tadeo <tadeo.mej@gmail.com>
2022-06-20 16:44:59 -0500

    AngularStore

---
+++ src/app/components/product.component.ts
@@ -0,0 +1,51 @@
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter,
+    OnChanges,
+    OnInit,
+    DoCheck,
+    OnDestroy,
+    SimpleChanges
+} from '@angular/core';
+import { Product } from '../product.model';
+
+@Component({
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.scss']
+})
+
+export class ProductComponent implements OnChanges, OnInit, DoCheck, OnDestroy {
+
+    @Input() product: Product;
+    @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    today = new Date();
+
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    ngOnChanges(changes: SimpleChanges) {
+        console.log('2. ngOnChanges', changes);
+    }
+
+    ngOnInit() {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. ngDoCheck');
+    }
+
+    ngOnDestroy() {
+        console.log('5. ngOnDestroy');
+    }
+
+    addCart() {
+        console.log('anadir al carrito');
+        this.productClicked.emit(this.product.id);
+    }
+}
